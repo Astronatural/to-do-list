6bf18d2dde2a325018030044a8c54b6687b1b5ca
JDK <jdkranzler@yahoo.com>
2021-04-29 15:09:29 -0500

    new DB

--- database.sql
+++ database.sql
@@ -6,28 +6,28 @@ CREATE TABLE "chores" (
 
 SELECT * FROM "chores" ORDER BY "id";
 
-INSERT INTO "chores" ("task", "status")
-VALUES('Wash the dishes', 'n');
+INSERT INTO "chores" ("task")
+VALUES('Wash the dishes');
 
 
-INSERT INTO "chores" ("task", "status")
-VALUES('Shovel the driveway', 'n');
+INSERT INTO "chores" ("task")
+VALUES('Shovel the driveway');
 
 
-INSERT INTO "chores" ("task", "status")
-VALUES('Feed the cat', 'n');
+INSERT INTO "chores" ("task")
+VALUES('Feed the cat');
 
 
-INSERT INTO "chores" ("task", "status")
-VALUES('Make dinner', 'n');
+INSERT INTO "chores" ("task")
+VALUES('Make dinner');
 
 
-INSERT INTO "chores" ("task", "status")
-VALUES('Buy groceries', 'n');
+INSERT INTO "chores" ("task")
+VALUES('Buy groceries');
 
 
-INSERT INTO "chores" ("task", "status")
-VALUES('Get the mail', 'n');
+INSERT INTO "chores" ("task")
+VALUES('Get the mail');
 
 
 INSERT INTO "chores" ("task", "status")
--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,7 +1,7 @@
 const pg = require('pg');
 
 const config = {
-    database: 'weekend-to-do-app',
+    database: 'BigToDoApp',
     host: 'localhost',
     port: 5432,
     max: 10,
